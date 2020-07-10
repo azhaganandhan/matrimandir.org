1c207cccbd60b6cf93fdc8171808238a9cea2982
Shankar Dhanasekaran <shankardevy@gmail.com>
2020-07-10 09:15:27 +0530

    Update email.ex

--- lib/matrimandir/email.ex
+++ lib/matrimandir/email.ex
@@ -3,7 +3,7 @@ defmodule Matrimandir.Email do
 
   def getting_in_touch_mail(name, email, message) do
     new_email(
-      to: "nittin.shankar@gmail.com",
+      to: "matrimandir@auroville.org.in",
       from: email,
       subject: name <> " has sent a message",
       html_body: "",
