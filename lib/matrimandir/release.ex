67a41a361fc1433ea7af97d0fde920f8dae2ba33
Azhaganandhan <anandpec99@gmail.com>
2020-07-21 10:19:38 +0530

    added dockerfile

---
+++ lib/matrimandir/release.ex
@@ -0,0 +1,18 @@
+defmodule Matrimandir.Release do
+  @app :matrimandir
+
+  def migrate do
+    for repo <- repos() do
+      {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.ensure_all_started(@app)
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+end
